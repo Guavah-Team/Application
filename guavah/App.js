363b8b9ba513989c2c0c2a1ab83a9114f1633a80
Dylan Guzman <dylanaguzman@gmail.com>
2022-03-01 14:03:11 -0800

    Navigation

--- guavah/App.js
+++ guavah/App.js
@@ -9,7 +9,7 @@ import NewPasswordScreen from './app/screens/NewPasswordScreen';
 import Navigation from './app/navigation';
 import HorizontalRestaurantPage from './app/components/HorizontalRestaurantBox/HorizontalRestaurantPage';
 import VerticalRestaurantBox from './app/components/VerticalRestaurantBox/VerticalRestaurantBox';
-import NavigationBar from './app/components/NavigationBar/NavigationBar';
+
 import HomeScreen from './app/screens/HomeScreen';
 
 
--- guavah/app/screens/HomeScreen/HomeScreen.js
+++ guavah/app/screens/HomeScreen/HomeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import NavigationBar from '../../components/NavigationBar/NavigationBar';
+
 
 function HomeScreen(props) {
     return (

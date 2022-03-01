ad7ec6e69f9622f13f6b21220853863d22b423ef
Ryan Miller <ryan@millers4c.net>
2022-02-28 17:02:46 -0800

    initial

---
+++ guavah/app/components/HorizontalUserData/HorizontalUserData.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import colors from '../../config/colors/colors';
+import { Auth } from 'aws-amplify';
+
+function HorizontalUserData({text}) {
+    return (
+        <View>
+
+            <Text>User Name</Text>
+        </View>
+        
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        flex: 1,
+
+        padding: 15,
+        marginVertical: 5,
+
+        alignItems: 'center',
+        borderRadius: 5,
+    },
+
+    container_PRIMARY: {
+        backgroundColor: colors.accent,
+        borderRadius: 100,
+    },
+    container_SECONDARY: {
+        borderColor: colors.accent,
+        borderWidth: 2,
+        borderRadius: 100,
+    },
+    container_TERTIARY: {
+        marginVertical: 2,
+    },
+
+    text: {
+        color: colors.dark,        
+
+    },
+    text_SECONDARY: {
+        color: colors.accent,
+    },
+    text_TERTIARY: {
+        color: 'gray',
+        // alignSelf: 'stretch',
+    }
+})
+
+export default HorizontalUserData;
--- guavah/app/screens/RestaurantScreen/RestaurantScreen.js
+++ guavah/app/screens/RestaurantScreen/RestaurantScreen.js
@@ -1,8 +1,30 @@
 import React, { useState } from 'react';
 import {StyleSheet, View, Text, ScrollView} from 'react-native';
+import colors from '../../config/colors/colors';
+import Wordmark from '../../components/Wordmark/Wordmark';
+import CustomInput from '../../components/CustomInput';
+import CustomButton from '../../components/CustomButton';
+import {useNavigation} from '@react-navigation/native';
+import { Auth } from 'aws-amplify';
 
 function RestaurantScreen(){
-
+    const navigation = useNavigation();
+    
+    
+    return(
+        <ScrollView>
+            <View  style = {styles.root}>
+            <Text style = {styles.root}>Olive Garden</Text>
+            </View>
+        </ScrollView>
+    )
 }
 
+const styles = StyleSheet.create({
+    root:{
+        flex: 1,
+        padding: 33,
+        alignItems: 'center',
+    },
+})
 export default RestaurantScreen;

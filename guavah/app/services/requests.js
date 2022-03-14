381e2097d2163f5c792e1302f561cd7d969dcc5d
Dylan Guzman <dylanaguzman@gmail.com>
2022-03-13 22:32:15 -0700

    updates

--- guavah/app/services/requests.js
+++ guavah/app/services/requests.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 export const getDetailedRestaurantData = async () => {
-    // url = "https://dt9tx0ox2d.execute-api.us-west-1.amazonaws.com/test/home?latlong=33.9806,-117.3755&radius=2000";
+    url = "https://dt9tx0ox2d.execute-api.us-west-1.amazonaws.com/test/home?latlong=33.9806,-117.3755&radius=2000";
       try{
           const response = await axios.get(url);
           let sections = [];
@@ -14,6 +14,9 @@ export const getDetailedRestaurantData = async () => {
           return sections;
       }catch (e) {
         console.log(e);
-      }
+    }
 };
 
+export const getSearchRestaurantData = async ({restaurantName}) => {
+  url = `https://dt9tx0ox2d.execute-api.us-west-1.amazonaws.com/test/search?radius=1600&latlong=33.9806,-117.3755&term=&category=13000&isOpen=false&doChains=true&minPrice=1&maxPrice=2&limit=10`
+}

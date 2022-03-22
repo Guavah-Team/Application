9d803e5a82f98079ed778004233873f01e9ebb83
Ryan Miller <ryan@millers4c.net>
2022-03-22 14:56:02 -0700

    both modal integrated

--- guavah/app/screens/ReviewScreen/ReviewScreen.js
+++ guavah/app/screens/ReviewScreen/ReviewScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {StyleSheet, View, Text, ScrollView, Image, ImageBackground, TextInput, Modal} from 'react-native';
+import {StyleSheet, View, Text, ScrollView, Image, ImageBackground, TextInput, Modal, Pressable, Alert} from 'react-native';
 import colors from '../../config/colors/colors';
 import Wordmark from '../../components/Wordmark/Wordmark';
 import CustomInput from '../../components/CustomInput';
@@ -9,44 +9,136 @@ import { Auth } from 'aws-amplify';
 import RestaurantReview from '../../components/RestaurantReview';
 
 function ReviewScreen(){
+    const [commentVisible, setCommentVisible] = useState(false);
+    const [thumbsVisible, setThumbsVisible] = useState(false);
+    const [randomVisible, setrandomVisible] = useState(false);
+
     return(
-        <View style = {styles.container}>
-            <Text style = {styles.text}>Leave a Comment</Text>
-            <TextInput style = {styles.input} placeholder = 'Optional' multiline = {true}/>
-            <View style = {styles.button}><CustomButton text = {'Submit'}/></View>
+        <View>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={commentVisible}
+                onRequestClose={() => {
+                Alert.alert("Modal has been closed.");
+                setCommentVisible(!commentVisible);
+                }}
+            >
+                <View style = {styles.container}>
+                    <View style = {styles.modalView}>
+                        <Text style = {styles.header}>Leave a Comment</Text>
+                        <TextInput style = {styles.input} placeholder = 'Optional' multiline = {true}/>
+                        <Pressable style = {styles.button} onPress={() => setCommentVisible(!commentVisible)}>
+                            <Text style = {styles.textStyle}>Submit</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </Modal>
+            <Modal
+                animationType="fade"
+                transparent={true}
+                visible = {thumbsVisible}
+                onRequestClose={() => {
+                    Alert.alert("Thumbs has been closed"); 
+                setThumbsVisible(!thumbsVisible);
+                }}
+            >
+                <View style = {styles.container}>
+                <View style = {styles.modalView}>
+                    <Pressable style = {styles.button} onPress={() => setThumbsVisible(!thumbsVisible)}>
+                        <Text style = {styles.textStyle}>close</Text>
+                    </Pressable>
+                    
+                </View>
+                </View>
+                
+            </Modal>
             
-            <Text>2 of 2</Text>
+            <Pressable
+                style={[styles.tempbutton, styles.buttonOpen]}
+                onPress={() => setCommentVisible(true)}
+            >
+                <Text style={styles.textStyle}>Write Review?</Text>
+            </Pressable>
+            <Pressable
+                style={[styles.tempbutton, styles.buttonOpen]}
+                onPress={() => setThumbsVisible(true)}
+            >
+                <Text style={styles.textStyle}>Thumbs</Text>
+            </Pressable>
         </View>
+        
     )
 }
 
 const styles = StyleSheet.create({
     container:{
-        flex: 1,
-        alignItems: 'center'
+        flex: .5,
+        justifyContent: 'center',
+        backgroundColor: colors.background
     },
     root:{
-        justifyContent: 'center'
+        
     },
-    center:{
+    modalView:{
+        backgroundColor: colors.background,
+        borderRadius: 20,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+    },
+    status:{
         alignItems: 'center',
         justifyContent: 'flex-end'
     },
-    text:{
+    header:{
         fontWeight: 'bold',
         textAlign: 'center',
-        paddingTop: '8%',
-        paddingBottom: '12%'
+        paddingBottom: '15%',
+        paddingTop: '10%'
+    },
+    base:{
+        justifyContent: 'flex-end'
     },
     input:{
-        width:'50%',
-        height: '15%',
+        width:'70%',
+        height: '40%',
         borderWidth: 1,
         borderColor: colors.light,
+        borderRadius: 5,
+        paddingBottom: '10%'
     },
     button:{
-        width: '50%',
-        height: '7%',
+        borderRadius: 30,
+        elevation: 2,
+        backgroundColor: colors.accent,
+        width: '70%',
+        height: 40,
+        alignItems: 'center',
+        justifyContent: 'center',
+        
+    },
+    tempbutton: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+        backgroundColor: colors.accent
+      },
+    buttons: {
+        paddingTop: '10%',
+        flex: 1,
+    },
+    buttonClose:{
+        backgroundColor: colors.accent,
+    },
+    buttonOpen: {
+        backgroundColor: colors.accent,
+    },
+    textStyle:{
+        color: colors.white,
     }
 })
 

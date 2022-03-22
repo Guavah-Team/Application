62b23fe3fad1e5791d8afc6fbaf62cb801200a01
Ryan Miller <ryan@millers4c.net>
2022-03-22 16:14:53 -0700

    switch

--- guavah/app/screens/ReviewScreen/ReviewScreen.js
+++ guavah/app/screens/ReviewScreen/ReviewScreen.js
@@ -11,7 +11,6 @@ import RestaurantReview from '../../components/RestaurantReview';
 function ReviewScreen(){
     const [commentVisible, setCommentVisible] = useState(false);
     const [thumbsVisible, setThumbsVisible] = useState(false);
-    const [randomVisible, setrandomVisible] = useState(false);
 
     return(
         <View>
@@ -26,15 +25,18 @@ function ReviewScreen(){
                 setCommentVisible(!commentVisible);
                 }}
             >
-                <View style = {styles.container}>
+                <View style = {styles.containerModal}>
                     <View style = {styles.modalView}>
                         <Text style = {styles.header}>Leave a Comment</Text>
                         <TextInput style = {styles.input} placeholder = 'Optional' multiline = {true}/>
-                        <Pressable style = {styles.button} onPress={() => setCommentVisible(!commentVisible)}>
-                            <Text style = {styles.textStyle}>Submit</Text>
+                        <View style = {styles.buttonView}>
+                        <Pressable style = {styles.buttonModal} onPress={() => setCommentVisible(!commentVisible)}>
+                            <Text style = {styles.textStyleModal}>Submit</Text>
                         </Pressable>
+                        </View>
+                        
                         <Pressable  onPress={() => setCommentVisible(false)}>
-                            <Text style = {styles.textStyleSecondary}>Skip</Text>
+                            <Text style = {styles.textStyleSecondaryModal}>Skip</Text>
                         </Pressable>
                     </View>
                 </View>
@@ -50,23 +52,23 @@ function ReviewScreen(){
                 setThumbsVisible(!thumbsVisible);
                 }}
             >
-                <View style = {styles.container}>
+                <View style = {styles.containerModal}>
                     <View style = {styles.modalView}>
-                        <Pressable style = {styles.button} onPress={() => setThumbsVisible(!thumbsVisible)}>
-                            <Text style = {styles.textStyle}>close</Text>
+                        <Pressable style = {styles.buttonModal} onPress={() => setThumbsVisible(!thumbsVisible)}>
+                            <Text style = {styles.textStyleModal}>close</Text>
                         </Pressable>
-                        <Pressable style={styles.button} onPress={() => {setCommentVisible(true); setThumbsVisible(false);}}>
-                            <Text style={styles.textStyle}>Write Review?</Text>
+                        <Pressable style={styles.buttonModal} onPress={() => {setCommentVisible(true); setThumbsVisible(false);}}>
+                            <Text style={styles.textStyleModal}>Write Review?</Text>
                         </Pressable>
                     </View>
                 </View>
             </Modal>
 
             <Pressable
-                style={styles.button}
+                style={styles.buttonModal}
                 onPress={() => setThumbsVisible(true)}
                 >
-                <Text style={styles.textStyle}>Review</Text>
+                <Text style={styles.textStyleModal}>Review</Text>
             </Pressable>
 
         </View>
@@ -75,42 +77,60 @@ function ReviewScreen(){
 }
 
 const styles = StyleSheet.create({
-    container:{
+    containerModal:{
         flex: 1,
         justifyContent: 'center',
-        backgroundColor: colors.background
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        alignItems: 'center',
+        paddingTop: '20%'
     },
     modalView:{
         backgroundColor: colors.background,
-        borderRadius: 20,
+        borderRadius: 5,
         alignItems: "center",
         shadowColor: "#000",
         shadowOffset: {width: 0, height: 2},
         shadowOpacity: 0.25,
         shadowRadius: 4,
-        elevation: 5
+        elevation: 5,
+        height: '50%',
+        width: '90%',
+
     },
     header:{
+        fontWeight: 'bold',
+        paddingTop: 40,
+        paddingBottom: 50,
         
     },
     input:{
-       
+       borderWidth: 1,
+       borderColor: 'gray',
+       borderRadius: 10,
+       width: '65%',
+       height: '25%',
     },
-    button:{
+    buttonModal:{
         borderRadius: 30,
         elevation: 2,
         backgroundColor: colors.accent,
-        width: '70%',
-        height: 40,
+        width: 250,
+        height: 50,
         alignItems: 'center',
-        justifyContent: 'center',
-        
+        justifyContent: 'center'
     },
-    textStyle:{
+    buttonView:{
+       
+        alignItems: 'center',
+        paddingTop: '20%',
+        flex: 1
+    },
+    textStyleModal:{
         color: colors.white,
     },
-    textStyleSecondary:{
-        color: 'gray'
+    textStyleSecondaryModal:{
+        color: 'gray',
+        paddingBottom: 10
     }
 })
 

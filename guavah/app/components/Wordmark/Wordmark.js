f11bab6119ba4c938ac87fbbe2a3e539c15fce43
Ryan Miller <ryan@millers4c.net>
2022-03-24 13:43:23 -0700

    done

--- guavah/app/components/Wordmark/Wordmark.js
+++ guavah/app/components/Wordmark/Wordmark.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import colors from '../../config/colors/colors';
 import {StyleSheet, View, Text} from 'react-native';
+import {useFonts} from 'expo-font';
 
 function Wordmark(props) {
+    const [loaded] = useFonts({
+        CeraBlack: require('../../assets/fonts/CeraPro-Black.otf'),
+        CeraBlackItalic: require('../../assets/fonts/CeraPro-BlackItalic.otf'),
+        CeraBold: require('../../assets/fonts/CeraPro-Bold.otf'),
+        CeraItalic: require('../../assets/fonts/CeraPro-Italic.otf'),
+        CeraLight: require('../../assets/fonts/CeraPro-Light.otf'),
+        CeraMedium: require('../../assets/fonts/CeraPro-Medium.otf'),
+
+      });
+
+    if (!loaded){
+        return null;
+    }
+
     return (
         <Text style = {styles.wordmark}>guavah</Text>
     );
 }
 
 const styles = StyleSheet.create({
-    wordmark: {
+    wordmark:{
         fontSize: 60,
-        fontWeight: '500',
-        color: colors.accent,
-    },
+        fontFamily: 'CeraMedium',
+        color: colors.accent
+    }
+    
 })
 
 export default Wordmark;

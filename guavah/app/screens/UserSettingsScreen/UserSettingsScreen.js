b2630608b6e8e2fb0888a15e9c7c79e11a8093aa
RMiller <ryan@millers4c.net>
2022-03-23 17:19:02 -0700

    just profile Picture

--- guavah/app/screens/UserSettingsScreen/UserSettingsScreen.js
+++ guavah/app/screens/UserSettingsScreen/UserSettingsScreen.js
@@ -1,18 +1,31 @@
 import React, { useState } from 'react';
-import {StyleSheet, View, Text, ScrollView, Alert, Pressable, SafeAreaView, TextInput} from 'react-native';
+import {StyleSheet, View, Text, Image, Alert, Pressable, SafeAreaView, TextInput, Switch} from 'react-native';
 import colors from '../../config/colors/colors';
 import CustomInput from '../../components/CustomInput';
 import CustomButton from '../../components/CustomButton';
 import {useNavigation} from '@react-navigation/native';
 import {useForm, Controller} from 'react-hook-form';
 import {Auth} from 'aws-amplify';
+import Slider from '@react-native-community/slider';
 
 const EMAIL_REGEX = /^[a-zA-Z0-9.!$#%&-]+@[a-zA-Z0-9]+\.[a-z]{2,3}/
 
 function UserSettingsScreen(){
+
+    const [range, setRange] = useState('0 Miles')
+    const [sliding, setSliding] = useState('Inactive')
+
+    const [isDarkEnabled, setIsDarkEnabled] = useState(false);
+    const toggleDarkSwitch = () => setIsDarkEnabled(previousState => !previousState);
+
+    const [isVeganEnabled, setIsVeganEnabled] = useState(false);
+    const toggleVeganSwitch = () => setIsVeganEnabled(previousState => !previousState);
+    
+
     return (
         <View style = {styles.container}>
 
+            {/* Header */}
             <View style = {styles.header}>
                 <SafeAreaView>
                     <Text style = {styles.textStyleHeader}>settings</Text>
@@ -22,29 +35,79 @@ function UserSettingsScreen(){
                 </Pressable>
             </View>
 
+            {/* Profile Box */}
             <Text style = {styles.sections}>Profile</Text>
             <View style = {styles.profileSettingsBox}>
             
-                <Text>Email</Text>
-                <TextInput placeholder='Email' rules = {{
-                            required: 'Email is required',
-                            pattern: {
-                                value: EMAIL_REGEX,
-                                message: 'Email is invalid',
-                            }
-                        }}/>
+                <Text style = {styles.profileText}>Name</Text>
+                <TextInput  
+                    style = {styles.input}
+                    value = {'Ryan Miller'}
+                    />
+                <Text style = {styles.profileText}>Email</Text>
+                <TextInput 
+                    style = {styles.input}
+                    value = {'ryanthomas.miller@calbaptist.edu'}
+                    />
             </View>
-            <Text style = {styles.sections}>Location</Text>
-            <View style = {styles.profileSettingsBox}>
 
-            </View>
+            {/* Location Box */}
+            <Text style = {styles.sections}>Location</Text>
+                <View style = {styles.profileSettingsBox}>
+                    <View style = {styles.locations}>
+                        <View style = {styles.locationView}><Image style = {styles.location} source={require('../../assets/userSettingsIcons/location.png')}/></View>
+                        <Text>Search Radius</Text>
+                        <View  style = {styles.switch}><Text>{range}</Text></View>
+                    </View>
+                    
+                    <Slider
+                        style={{width: '90%', height: 40, alignSelf: 'center', flex: .1}}
+                        minimumValue={0}
+                        maximumValue={1}
+                        thumbTintColor= {colors.accent}
+                        minimumTrackTintColor={colors.accent}
+                        maximumTrackTintColor= "#1b1b1b"
+                        onValueChange={value => setRange(parseInt(value * 15.9) + " miles")}
+                    />
+                </View>
+
+            {/* Preference Box */}
             <Text style = {styles.sections}>Preferences</Text>
             <View style = {styles.profileSettingsBox}>
-
+                <View style = {styles.preferences}>
+                    <View style = {styles.moonView}><Image style = {styles.moon} source={require('../../assets/userSettingsIcons/DarkMode.png')}/></View>
+                    
+                    <Text>Dark Theme (cs approved)</Text>
+                    <View style = {styles.switch}>
+                        <Switch
+                            trackColor={{ false: "#1b1b1b" , true: "#1b1b1b" }}
+                            thumbColor={isDarkEnabled ? colors.accent : colors.white}
+                            ios_backgroundColor = "#1b1b1b"
+                            onValueChange={toggleDarkSwitch}
+                            value={isDarkEnabled}
+                        />
+                    </View>
+                </View>
+                <View style = {styles.preferences}>
+                <View style = {styles.plantView}><Image style = {styles.plant} source={require('../../assets/userSettingsIcons/VeganPrio.png')}/></View>
+                    <Text>Vegan Prioritization</Text>
+                    <View style = {styles.switch}>
+                        <Switch
+                            trackColor={{ false: "#1b1b1b" , true: "#1b1b1b" }}
+                            thumbColor={isVeganEnabled ? colors.accent : colors.white}
+                            ios_backgroundColor = "#1b1b1b"
+                            onValueChange={toggleVeganSwitch}
+                            value={isVeganEnabled}
+                        />
+                    </View>
+                </View>
+            
             </View>
-            <Text style = {styles.sections}>More</Text>
-            <View style = {styles.profileSettingsBox}>
 
+            {/* Rate Box */}
+            <Text style = {styles.sections}>More</Text>
+            <View style = {styles.rate}>
+                <Pressable style = {styles.ratePress} onPress={() => {alert('Redirecting to App Store');}}><Text>Rate our app</Text></Pressable>
             </View>
 
             <View style = {styles.save}>
@@ -64,7 +127,7 @@ const styles = StyleSheet.create({
     backButton:{
         position: 'absolute',
         left: 20,
-        top: '45%'
+        top: 55
     },
     backButtonText:{
         fontSize: 30,
@@ -73,7 +136,7 @@ const styles = StyleSheet.create({
     },
     header:{
         backgroundColor: colors.accent,
-        height: '15%',
+        height: 125,
         alignItems: 'center'
     },
     save:{
@@ -85,8 +148,8 @@ const styles = StyleSheet.create({
     },
     button: {
         backgroundColor: colors.accent,
-        width: '50%',
-        height: 40,
+        width: 200,
+        height: 50,
         justifyContent: 'center',
         alignItems: 'center',
         borderRadius: 30
@@ -107,8 +170,8 @@ const styles = StyleSheet.create({
     },
     profileSettingsBox:{
         
-        width: '90%',
-        height: '10%',
+        width: 350,
+        height: 120,
         alignSelf: 'center',
         shadowColor: '#171717',
         shadowOffset: {width: 0, height: 4},
@@ -118,6 +181,75 @@ const styles = StyleSheet.create({
         backgroundColor: colors.background,
         borderRadius: 5
     },
+    rate:{
+        width: 350,
+        height: 40,
+        alignSelf: 'center',
+        shadowColor: '#171717',
+        shadowOffset: {width: 0, height: 4},
+        shadowOpacity: 0.3,
+        shadowRadius: 2,
+        elevation: 4,
+        backgroundColor: colors.background,
+        borderRadius: 5,
+        justifyContent: 'center',
+        paddingLeft: 20,
+    },
+    ratePress:{
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center'
+    },
+    preferences:{
+        flexDirection: 'row',
+        flex: .5,
+        alignItems: 'center'
+    },
+    switch:{
+        alignItems: 'flex-end',
+        flex: 1,
+        paddingRight: 10
+    },
+    moon:{
+        width: 30,
+        height: 30,
+        
+    },
+    moonView:{
+        paddingHorizontal: 10
+    },
+    plant:{
+        width: 30,
+        height: 21,
+        
+    },
+    plantView:{
+        paddingHorizontal: 10
+    },
+    location:{
+        width: 30,
+        height: 30,
+    },
+    locationView:{
+        paddingHorizontal: 10
+    },
+    locations:{
+        flexDirection: 'row',
+        flex: .7,
+        alignItems: 'center'
+    },
+    input:{
+        borderBottomColor: colors.light,
+        borderWidth: 1,
+        borderTopColor: colors.background,
+        borderLeftColor: colors.background,
+        borderRightColor: colors.background,
+        width: 220
+    },
+    profileText:{
+        color: "gray"
+    }
+   
 })
 
 export default UserSettingsScreen;
